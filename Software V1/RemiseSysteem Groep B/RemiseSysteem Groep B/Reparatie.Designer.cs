7294474378ee88632debe10df514c7f0baee4661
thijnofant <tavandijk@live.nl>
2014-10-15 10:34:09 +0200

    ?

--- Software V1/RemiseSysteem Groep B/RemiseSysteem Groep B/Reparatie.Designer.cs
+++ Software V1/RemiseSysteem Groep B/RemiseSysteem Groep B/Reparatie.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Reparatie
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Reparatie";
+            this.ClientSize = new System.Drawing.Size(284, 260);
+            this.Name = "Reparatie";
+            this.Text = "reparatie";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
