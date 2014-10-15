c8a2d4d38a109e03a56cbb63d01d5fdad07b76cd
thijnofant <tavandijk@live.nl>
2014-10-15 14:15:31 +0200

    Beurt inherit fix. onderhouds aplicatie error fix

--- Software V1/RemiseSysteem Groep B/RemiseSysteem Groep B/Beurt.cs
+++ Software V1/RemiseSysteem Groep B/RemiseSysteem Groep B/Beurt.cs
@@ -8,20 +8,21 @@ namespace RemiseSysteem_Groep_B
 {
     abstract class Beurt
     {
-        private DateTime BeginDatum;
         private int ID;
         private BeurtType Soort;
         private bool IsKlaar;
 
-        private List<Medewerker> Medewerkers;
+        public List<Medewerker> Medewerkers { get; private set; }
 
-        private Tram Tram;
+        public Tram Tram { get; protected set; }
+        public DateTime BeginDatum { get; protected set; }
 
         public Beurt(DateTime beginDatum, int id, BeurtType soort)
         {
             this.BeginDatum = beginDatum;
             this.ID = id;
             this.Soort = soort;
+            Medewerkers = new List<Medewerker>();
         }
 
         public abstract List<Medewerker> MedewerkersOpvragen();
