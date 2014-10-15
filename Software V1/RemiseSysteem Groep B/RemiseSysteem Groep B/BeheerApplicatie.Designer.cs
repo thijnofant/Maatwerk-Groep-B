1c852d723724087b3c1a3f28d9055eaceedcaba9
EmonR <emon.raijmakers@student.fontys.nl>
2014-10-15 13:46:58 +0200

    Wijzigingen form Beheerapplicatie

--- Software V1/RemiseSysteem Groep B/RemiseSysteem Groep B/BeheerApplicatie.Designer.cs
+++ Software V1/RemiseSysteem Groep B/RemiseSysteem Groep B/BeheerApplicatie.Designer.cs
@@ -28,12 +28,39 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.toolStripContainer1 = new System.Windows.Forms.ToolStripContainer();
+            this.toolStripContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStripContainer1
+            // 
+            // 
+            // toolStripContainer1.ContentPanel
+            // 
+            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(1283, 2);
+            this.toolStripContainer1.Location = new System.Drawing.Point(3, 1);
+            this.toolStripContainer1.Name = "toolStripContainer1";
+            this.toolStripContainer1.Size = new System.Drawing.Size(1283, 27);
+            this.toolStripContainer1.TabIndex = 0;
+            this.toolStripContainer1.Text = "toolStripContainer1";
+            // 
+            // BeheerApplicatie
+            // 
+            this.ClientSize = new System.Drawing.Size(1287, 526);
+            this.Controls.Add(this.toolStripContainer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "BeheerApplicatie";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.toolStripContainer1.ResumeLayout(false);
+            this.toolStripContainer1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ToolStripContainer toolStripContainer1;
     }
 }
 
