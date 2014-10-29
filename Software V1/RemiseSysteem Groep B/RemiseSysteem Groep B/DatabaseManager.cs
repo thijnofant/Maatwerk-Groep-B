926f24faead2c9455a9ffdaf3b60ae5fadc7ca46
123 <robert.horvers@gmail.com>
2014-10-29 12:35:36 +0100

    Revert "Oracleclient reference"
    This reverts commit ecb820c0315e422a70391d7f955c1c4db6f62b61.

--- Software V1/RemiseSysteem Groep B/RemiseSysteem Groep B/DatabaseManager.cs
+++ Software V1/RemiseSysteem Groep B/RemiseSysteem Groep B/DatabaseManager.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Oracle.DataAccess.Client;
-using System.Data.OracleClient;
 
 namespace RemiseSysteem_Groep_B
 {
@@ -15,6 +14,7 @@ namespace RemiseSysteem_Groep_B
         private static DatabaseManager instance;
         private DatabaseManager() 
         {
+            this.connection = new OracleConnection();
             this.Pcn = "dbi292195";
             this.Password = "kd1qoIM98M";
             connection.ConnectionString = "User Id=" + this.Pcn + ";Password=" + this.Password + ";Data Source=" + "//192.168.15.50:1521/fhictora;";
