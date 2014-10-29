b21b1a1dd5889b7aed0cd9022a739e43455efca5
thijnofant <tavandijk@live.nl>
2014-10-29 10:36:53 +0100

    DB Inlog

--- Software V1/RemiseSysteem Groep B/RemiseSysteem Groep B/DatabaseManager.cs
+++ Software V1/RemiseSysteem Groep B/RemiseSysteem Groep B/DatabaseManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Oracle.DataAccess;
 
 namespace RemiseSysteem_Groep_B
 {
@@ -12,10 +13,9 @@ namespace RemiseSysteem_Groep_B
         private static DatabaseManager instance;
         private DatabaseManager() 
         {
-            this.Pcn = "********";
-            this.Password = "********";
-            //TODO: Fix
-            //connection.ConnectionString = "User Id=" + this.Pcn + ";Password=" + this.Password + ";Data Source=" + "//192.168.15.50:1521/fhictora;";
+            this.Pcn = "dbi292195";
+            this.Password = "kd1qoIM98M";
+            connection.ConnectionString = "User Id=" + this.Pcn + ";Password=" + this.Password + ";Data Source=" + "//192.168.15.50:1521/fhictora;";
         }
         public static DatabaseManager Instance
         {
