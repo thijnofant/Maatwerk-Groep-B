37f0c802ba75d773c19e0a650edefbecfd61fb7f
EmonR <emon.raijmakers@student.fontys.nl>
2014-10-29 12:29:44 +0100

    new oracleconnection

--- Software V1/RemiseSysteem Groep B/RemiseSysteem Groep B/DatabaseManager.cs
+++ Software V1/RemiseSysteem Groep B/RemiseSysteem Groep B/DatabaseManager.cs
@@ -14,6 +14,7 @@ namespace RemiseSysteem_Groep_B
         private static DatabaseManager instance;
         private DatabaseManager() 
         {
+            this.connection = new OracleConnection();
             this.Pcn = "dbi292195";
             this.Password = "kd1qoIM98M";
             connection.ConnectionString = "User Id=" + this.Pcn + ";Password=" + this.Password + ";Data Source=" + "//192.168.15.50:1521/fhictora;";
