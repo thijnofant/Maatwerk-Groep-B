c5be57daa26f1752fd77ac5107d8952439a95807
thijnofant <tavandijk@live.nl>
2014-10-29 13:04:00 +0100

    changes in db manager and deleted some references

--- Software V1/RemiseSysteem Groep B/RemiseSysteem Groep B/DatabaseManager.cs
+++ Software V1/RemiseSysteem Groep B/RemiseSysteem Groep B/DatabaseManager.cs
@@ -4,7 +4,6 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Oracle.DataAccess.Client;
 using System.Data.OracleClient;
 
 namespace RemiseSysteem_Groep_B
