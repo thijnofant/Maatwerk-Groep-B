0aa354417f05ce50c3d342a68feb8363ab27c248
thijnofant <tavandijk@live.nl>
2014-10-31 16:00:11 +0100

    Database Get Clean List

--- Software V1/RemiseSysteem Groep B/RemiseSysteem Groep B/DatabaseManager.cs
+++ Software V1/RemiseSysteem Groep B/RemiseSysteem Groep B/DatabaseManager.cs
@@ -12,7 +12,7 @@ namespace RemiseSysteem_Groep_B
     {
         #region singleton
         private static DatabaseManager instance;
-        private DatabaseManager() 
+        private DatabaseManager()
         {
             this.connection = new OracleConnection();
             this.Pcn = "dbi292195";
@@ -21,13 +21,14 @@ namespace RemiseSysteem_Groep_B
         }
         public static DatabaseManager Instance
         {
-            get 
-            { 
+            get
+            {
                 if (instance == null)
                 {
                     instance = new DatabaseManager();
                 }
-            return instance;}
+                return instance;
+            }
         }
         #endregion
 
@@ -91,6 +92,12 @@ namespace RemiseSysteem_Groep_B
             return onderhoudsBeurten;
         }
 
+        /// <summary>
+        /// NOG TESTEN
+        /// 
+        /// Haalt de lijst met alle schoonmaak die niet nog niet af zijn.
+        /// </summary>
+        /// <returns></returns>
         public List<Schoonmaak> SchoonmaakOpvragen()
         {
             List<Schoonmaak> returnList = new List<Schoonmaak>();
@@ -102,25 +109,46 @@ namespace RemiseSysteem_Groep_B
             {
                 connection.Open();
                 OracleDataReader reader = command.ExecuteReader();
-                reader.Read();
-                int FoundStatus = reader.GetInt32(0);
-                int MedewerkerId = reader.GetInt32(1);
-                int tramId = reader.GetInt32(2);
-                DateTime startTijd = reader.GetDateTime(3);
-                string beurtType = reader["BeurtType"].ToString();
-                
-                /*
-                TramType tramtype = new TramType(FoundDescription, 1);
-                Tram tram = new Tram(ID, tramtype);
-                TramStatus tramtypeStatus = (TramStatus)Enum.Parse(typeof(TramStatus), FoundDescription, true);
-                tram.Status = tramtypeStatus; */
+                while(reader.Read())
+                {
+                    int SchoonmaakID = reader.GetInt32(0);
+                    int MedewerkerId = reader.GetInt32(1);
+                    int tramId = reader.GetInt32(2);
+                    DateTime startTijd = reader.GetDateTime(3);
+                    string beurtType = reader["BeurtType"].ToString();
+
+                    BeurtType tempEnum;
+
+                    switch (beurtType)
+                    {
+                        case "Klein":
+                            tempEnum = BeurtType.Kleine;
+                            break;
+                        case "Groot":
+                            tempEnum = BeurtType.Grote;
+                            break;
+                        case "Incident":
+                            tempEnum = BeurtType.Incident;
+                            break;
+                    }
+
+                    Tram tempTram = ZoekTramOpID(tramId);
+                    Medewerker tempMed = ZoekMedewerkerOpID(MedewerkerId);
+                    
+                    Schoonmaak tempSchoon = new Schoonmaak(startTijd, SchoonmaakID, tempEnum, tempTram);
+                    if (tempMed != null)
+                    {
+                        tempSchoon.VoegMedewerkerToe(tempMed);
+                    }
+                    returnList.Add(tempSchoon);
+                }
             }
             catch { }
             finally
             {
                 connection.Close();
             }
-            return returnList;                
+            return returnList;
         }
 
         /// <summary>
@@ -131,7 +159,7 @@ namespace RemiseSysteem_Groep_B
         {
             DataTable sporenlijst = new DataTable();
 
-            
+
             //TODO: SQL
             return sporenlijst;
         }
@@ -173,7 +201,84 @@ namespace RemiseSysteem_Groep_B
                 tram.Status = tramStatus;
                 return tram;
 
-                
+
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+                this.connection.Close();
+            }
+            return null;
+        }
+
+        public Tram ZoekTramOpID(int id)
+        {
+            String cmd = "Select t.*, tt.* From TRAM t, TRAMTYPE tt Where t.ID = '" + id + "' AND t.TramtypeID = tt.ID";
+            OracleCommand command = new OracleCommand(cmd, connection);
+            command.CommandType = System.Data.CommandType.Text;
+            try
+            {
+                this.connection.Open();
+
+                OracleDataReader reader = command.ExecuteReader();
+                reader.Read();
+                string GevondenStatus = reader["Status"].ToString();
+                string GevondenDescription = reader["Omschrijving"].ToString();
+
+                //aanvullen
+                TramType tramtype = new TramType(GevondenDescription, 1);
+                Tram tram = new Tram(id, tramtype);
+                TramStatus tramStatus = (TramStatus)Enum.Parse(typeof(TramStatus), GevondenStatus, true);
+                tram.Status = tramStatus;
+                return tram;
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+                this.connection.Close();
+            }
+            return null;
+        }
+
+        public Medewerker ZoekMedewerkerOpID(int id)
+        {
+            String cmd = "Select M.ID, M.Naam, F.Naam FROM MEDEWERKER M INNER JOIN FUNCTIE F ON M.FUNCTIEID = F.ID WHERE M.ID =" + id.ToString();
+            OracleCommand command = new OracleCommand(cmd, connection);
+            command.CommandType = System.Data.CommandType.Text;
+            try
+            {
+                this.connection.Open();
+
+                OracleDataReader reader = command.ExecuteReader();
+                reader.Read();
+                int medewerkerID = reader.GetInt32(0);
+                string medewerkerNaam = reader["M.Naam"].ToString();
+                string funtieNaam = reader["F.Naam"].ToString();
+
+                MedewerkerType temp;
+                switch (funtieNaam)
+                {
+                    case "Beheerder":
+                        temp = MedewerkerType.Beheerder;
+                        break;
+                    case "Bestuurder":
+                        temp = MedewerkerType.Bestuurder;
+                        break;
+                    case "Schoonmaker":
+                        temp = MedewerkerType.Schoonmaker;
+                        break;
+                    case "Technicus":
+                        temp = MedewerkerType.Technicus;
+                        break;
+                }
+                Medewerker returnmed = new Medewerker(medewerkerID, medewerkerNaam, temp);
+                return returnmed;
             }
             catch
             {
