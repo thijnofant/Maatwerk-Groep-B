b885ab8f5bfd73999a194bf10041a6fdfbf42c8a
i309262 <Sayalaugs@hotmail.com>
2014-11-04 13:08:56 +0100

    cool

--- Software V1/RemiseSysteem Groep B/RemiseSysteem Groep B/TramReserveren.cs
+++ Software V1/RemiseSysteem Groep B/RemiseSysteem Groep B/TramReserveren.cs
@@ -12,9 +12,20 @@ namespace RemiseSysteem_Groep_B
 {
     public partial class TramReserveren : Form
     {
+        DatabaseManager db = DatabaseManager.Instance;
         public TramReserveren()
         {
             InitializeComponent();
+        }
+
+        private void btnBevestig_Click(object sender, EventArgs e)
+        {
+            db.TramReserveren(Convert.ToInt32(tbTramnummer.Text), Convert.ToInt32(tbSpoornummer.Text));
+            
+            if(chbReparatie.Checked)
+            {
+                Onderhoud onderhoud = new Onderhoud(DateTime.Now, )
+            }
         }
     }
 }
