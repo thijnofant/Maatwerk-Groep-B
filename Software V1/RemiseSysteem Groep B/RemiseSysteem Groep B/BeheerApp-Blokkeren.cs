2c57dff3cc169f383d8a359156897786557b43e9
123 <robert.horvers@gmail.com>
2014-11-05 14:51:40 +0100

    Nieuw form

---
+++ Software V1/RemiseSysteem Groep B/RemiseSysteem Groep B/BeheerApp-Blokkeren.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RemiseSysteem_Groep_B
+{
+    public partial class BeheerApp_Blokkeren : Form
+    {
+        public BeheerApp_Blokkeren() {
+            InitializeComponent();
+        }
+    }
+}
