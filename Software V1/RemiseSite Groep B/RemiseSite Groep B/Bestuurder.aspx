621a0c53f91821fd286fe4ace6a18991a6c159c0
i309262 <Sayalaugs@hotmail.com>
2015-01-04 16:25:14 +0100

    bestuurderssysteem gemaakt

--- Software V1/RemiseSite Groep B/RemiseSite Groep B/Bestuurder.aspx
+++ Software V1/RemiseSite Groep B/RemiseSite Groep B/Bestuurder.aspx
@@ -1,3 +1,41 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Bestuurder.aspx.cs" Inherits="RemiseSite_Groep_B.Bestuurder" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    ga naar spoor<br />
+    <asp:Label ID="lblGoToTrack" runat="server" Text="..."></asp:Label>
+    <br />
+    <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <asp:TextBox ID="tbInput" runat="server" Height="37px" Width="99px"></asp:TextBox>
+    <br />
+    <asp:Button ID="btnOne" runat="server" OnClick="btnOne_Click" Text="1" Width="65px" />
+&nbsp;&nbsp;&nbsp;
+    <asp:Button ID="btnTwo" runat="server" OnClick="btnTwo_Click" Text="2" Width="65px" />
+&nbsp;&nbsp;&nbsp;
+    <asp:Button ID="btnThree" runat="server" OnClick="btnThree_Click" Text="3" Width="65px" />
+    <br />
+    <asp:Button ID="btnFour" runat="server" OnClick="btnFour_Click" Text="4" Width="65px" />
+&nbsp;&nbsp;&nbsp;
+    <asp:Button ID="btnFive" runat="server" OnClick="btnFive_Click" Text="5" Width="65px" />
+&nbsp;&nbsp;&nbsp;
+    <asp:Button ID="btnSix" runat="server" OnClick="btnSix_Click" Text="6" Width="65px" />
+    <br />
+    <asp:Button ID="btnSeven" runat="server" OnClick="btnSeven_Click" Text="7" Width="65px" />
+&nbsp;&nbsp;&nbsp;
+    <asp:Button ID="btnEight" runat="server" OnClick="btnEight_Click" Text="8" Width="65px" />
+&nbsp;&nbsp;&nbsp;
+    <asp:Button ID="btnNine" runat="server" OnClick="btnNine_Click" Text="9" Width="65px" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <asp:CheckBox ID="cbMaintenance" runat="server" Text="Onderhoud nodig" />
+    <br />
+    <asp:Button ID="btnBackspace" runat="server" OnClick="btnBackspace_Click" Text="herstel" Width="65px" />
+&nbsp;&nbsp;&nbsp;
+    <asp:Button ID="btnZero" runat="server" OnClick="btnZero_Click" Text="0" Width="65px" />
+&nbsp;&nbsp;&nbsp;
+    <asp:Button ID="btnConfirm" runat="server" OnClick="btnConfirm_Click" Text="Bevestig" Width="65px" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <asp:CheckBox ID="cbCleaning" runat="server" Text="schoonmaak nodig" />
+    <br />
+    <br />
+    <asp:Label ID="lblMessage" runat="server" Text="..."></asp:Label>
+    <br />
 </asp:Content>
--- Software V1/RemiseSite Groep B/RemiseSite Groep B/Bestuurder.aspx.cs
+++ Software V1/RemiseSite Groep B/RemiseSite Groep B/Bestuurder.aspx.cs
@@ -9,9 +9,107 @@ namespace RemiseSite_Groep_B
 {
     public partial class Bestuurder : System.Web.UI.Page
     {
+        //DatabaseManager db = DatabaseManager.Instance;
+        //Remise remise = Remise.Instance;
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void btnOne_Click(object sender, EventArgs e)
+        {
+            tbInput.Text = tbInput.Text += "1";//voert cijfer 1 in in invoerveld
+        }
+
+        protected void btnTwo_Click(object sender, EventArgs e)
+        {
+            tbInput.Text = tbInput.Text += "2";//voert cijfer 2 in in invoerveld
+        }
+
+        protected void btnThree_Click(object sender, EventArgs e)
+        {
+            tbInput.Text = tbInput.Text += "3";//voert cijfer 3 in in invoerveld
+        }
+
+        protected void btnFour_Click(object sender, EventArgs e)
+        {
+            tbInput.Text = tbInput.Text += "4";//voert cijfer 4 in in invoerveld
+        }
+
+        protected void btnFive_Click(object sender, EventArgs e)
+        {
+            tbInput.Text = tbInput.Text += "5";//voert cijfer 5 in in invoerveld
+        }
+
+        protected void btnSix_Click(object sender, EventArgs e)
+        {
+            tbInput.Text = tbInput.Text += "6";//voert cijfer 6 in in invoerveld
+        }
+
+        protected void btnSeven_Click(object sender, EventArgs e)
+        {
+            tbInput.Text = tbInput.Text += "7";//voert cijfer 7 in in invoerveld
+        }
+
+        protected void btnEight_Click(object sender, EventArgs e)
+        {
+            tbInput.Text = tbInput.Text += "8";//voert cijfer 8 in in invoerveld
+        }
+
+        protected void btnNine_Click(object sender, EventArgs e)
+        {
+            tbInput.Text = tbInput.Text += "9";//voert cijfer 9 in in invoerveld
+        }
+
+        protected void btnZero_Click(object sender, EventArgs e)
+        {
+            tbInput.Text = tbInput.Text += "0";//voert cijfer 0 in in invoerveld
+        }
+
+        protected void btnBackspace_Click(object sender, EventArgs e)
+        {
+            if (tbInput.Text.Length > 0)
+            {
+                tbInput.Text = tbInput.Text.Remove(tbInput.Text.Length - 1);// verwijderd laatst ingevoerde cijfer uit het invoerveld
+            }
+        }
+
+        protected void btnConfirm_Click(object sender, EventArgs e)
+        {
+            bool onderhoud = false;
+            bool schoonmaak = false;
+            int tramnr = Convert.ToInt32(tbInput.Text);
+
+            if (cbMaintenance.Checked)//wanneer er voor onderhoud is gekozen
+            {
+                onderhoud = true;
+            }
+            if (cbCleaning.Checked)//wanneer er voor schoonhoud is gekozen
+            {
+                schoonmaak = true;
+            }
+
+            if (db.ZoekTram(tramnr) != null)//kijkt of de tram bestaat, anders foutmelding
+            {
+                if (remise.PlaatsAutomatischToewijzen(tramnr, onderhoud, schoonmaak))//roept het algoritme aan
+                {
+                    if (db.GetToegewezenSpoor(db.ZoekTram(tramnr).Id) != 0)//kijkt of er een toegeweze spoor is
+                    {
+                        lblGoToTrack.Text = Convert.ToString(db.GetToegewezenSpoor(db.ZoekTram(tramnr).Id));//geeft het toegewezen spoor weer
+                    }
+                    else//wanneer er geen toegeweze spoor is komt de volgende error
+                    {
+                        lblMessage.Text = "Het systeem heeft geen spoor kunnen toekennen";
+                    }
+                }
+            }
+            else//error wanneer ongeldig tramnr wordt ingevoerd
+            {
+                lblMessage.Text = "invoer is geen geldige tram";
+            }
+
+        }
         }
     }
 }
