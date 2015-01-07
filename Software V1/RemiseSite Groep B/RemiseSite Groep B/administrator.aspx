541494c9aa19cce8a16cd9677a8fe4c3c2a826b0
EmonR <emon.raijmakers@student.fontys.nl>
2015-01-07 09:50:15 +0100

    beheerpagina aangepast

--- Software V1/RemiseSite Groep B/RemiseSite Groep B/administrator.aspx
+++ Software V1/RemiseSite Groep B/RemiseSite Groep B/administrator.aspx
@@ -1,9 +1,59 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="administrator.aspx.cs" Inherits="RemiseSite_Groep_B.administrator" %>
+
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
     <asp:UpdatePanel ID="upPlattegrond" runat="server">
         <ContentTemplate>
-            <asp:ListView ID="lvSporen" runat="server">
-            </asp:ListView>
+            <asp:UpdatePanel ID="upSporen" runat="server" UpdateMode="Conditional">
+                <ContentTemplate>
+                    <asp:Table ID="tableSporen1" runat="server" BorderStyle="None">
+                        <asp:TableRow ID="rowSporen1">
+                            <asp:TableCell ID="CellSpoor38" runat="server"></asp:TableCell>
+                            <asp:TableCell ID="CellSpoor37" runat="server"></asp:TableCell>
+                            <asp:TableCell ID="CellSpoor36" runat="server"></asp:TableCell>
+                            <asp:TableCell ID="CellSpoor35" runat="server"></asp:TableCell>
+                            <asp:TableCell ID="CellSpoor34" runat="server"></asp:TableCell>
+                            <asp:TableCell ID="CellSpoor33" runat="server"></asp:TableCell>
+                            <asp:TableCell ID="CellSpoor32" runat="server"></asp:TableCell>
+                            <asp:TableCell ID="CellSpoor31" runat="server"></asp:TableCell>
+                            <asp:TableCell ID="CellSpoor30" runat="server"></asp:TableCell>
+                            <asp:TableCell runat="server" Width="20"></asp:TableCell>
+                            <asp:TableCell ID="CellSpoor40" runat="server"></asp:TableCell>
+                            <asp:TableCell ID="CellSpoor41" runat="server"></asp:TableCell>
+                            <asp:TableCell ID="CellSpoor42" runat="server"></asp:TableCell>
+                            <asp:TableCell ID="CellSpoor58" runat="server"></asp:TableCell>
+                        </asp:TableRow>
+                    </asp:Table>
+                    <asp:Table ID="tableSporen2" runat="server" BorderStyle="None">
+                        <asp:TableRow ID="rowSporen2">
+                            <asp:TableCell ID="CellSpoor57" runat="server">
+                                <asp:Table ID="TableSpoor38" runat="server" BorderStyle="Solid" BorderWidth="1"></asp:Table>
+                            </asp:TableCell>
+                            <asp:TableCell ID="CellSpoor56" runat="server"></asp:TableCell>
+                            <asp:TableCell ID="CellSpoor55" runat="server"></asp:TableCell>
+                            <asp:TableCell ID="CellSpoor54" runat="server"></asp:TableCell>
+                            <asp:TableCell ID="CellSpoor53" runat="server"></asp:TableCell>
+                            <asp:TableCell ID="CellSpoor52" runat="server"></asp:TableCell>
+                            <asp:TableCell ID="CellSpoor51" runat="server"></asp:TableCell>
+                            <asp:TableCell ID="CellSpoor64" runat="server"></asp:TableCell>
+                            <asp:TableCell ID="CellSpoor63" runat="server"></asp:TableCell>
+                            <asp:TableCell ID="CellSpoor62" runat="server"></asp:TableCell>
+                            <asp:TableCell ID="CellSpoor61" runat="server"></asp:TableCell>
+                            <asp:TableCell runat="server" Width="10"></asp:TableCell>
+                            <asp:TableCell ID="CellSpoor74" runat="server"></asp:TableCell>
+                            <asp:TableCell ID="CellSpoor75" runat="server"></asp:TableCell>
+                            <asp:TableCell ID="CellSpoor76" runat="server"></asp:TableCell>
+                            <asp:TableCell ID="CellSpoor77" runat="server"></asp:TableCell>
+                            <asp:TableCell runat="server" Width="20"></asp:TableCell>
+                            <asp:TableCell ID="CellExtraSporen1" runat="server"></asp:TableCell>
+                            <asp:TableCell ID="CellExtraSporen2" runat="server"></asp:TableCell>
+                        </asp:TableRow>
+                    </asp:Table>
+                </ContentTemplate>
+                <Triggers>
+                    <asp:AsyncPostBackTrigger ControlID="Timer1" />
+                </Triggers>
+            </asp:UpdatePanel>
+            <asp:Timer ID="Timer1" runat="server" Interval="500"></asp:Timer>
         </ContentTemplate>
     </asp:UpdatePanel>
 </asp:Content>
--- Software V1/RemiseSite Groep B/RemiseSite Groep B/administrator.aspx.designer.cs
+++ Software V1/RemiseSite Groep B/RemiseSite Groep B/administrator.aspx.designer.cs
@@ -22,12 +22,336 @@ namespace RemiseSite_Groep_B {
         protected global::System.Web.UI.UpdatePanel upPlattegrond;
         
         /// <summary>
-        /// lvSporen control.
+        /// upSporen control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ListView lvSporen;
+        protected global::System.Web.UI.UpdatePanel upSporen;
+        
+        /// <summary>
+        /// tableSporen1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Table tableSporen1;
+        
+        /// <summary>
+        /// rowSporen1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableRow rowSporen1;
+        
+        /// <summary>
+        /// CellSpoor38 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell CellSpoor38;
+        
+        /// <summary>
+        /// CellSpoor37 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell CellSpoor37;
+        
+        /// <summary>
+        /// CellSpoor36 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell CellSpoor36;
+        
+        /// <summary>
+        /// CellSpoor35 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell CellSpoor35;
+        
+        /// <summary>
+        /// CellSpoor34 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell CellSpoor34;
+        
+        /// <summary>
+        /// CellSpoor33 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell CellSpoor33;
+        
+        /// <summary>
+        /// CellSpoor32 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell CellSpoor32;
+        
+        /// <summary>
+        /// CellSpoor31 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell CellSpoor31;
+        
+        /// <summary>
+        /// CellSpoor30 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell CellSpoor30;
+        
+        /// <summary>
+        /// CellSpoor40 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell CellSpoor40;
+        
+        /// <summary>
+        /// CellSpoor41 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell CellSpoor41;
+        
+        /// <summary>
+        /// CellSpoor42 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell CellSpoor42;
+        
+        /// <summary>
+        /// CellSpoor58 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell CellSpoor58;
+        
+        /// <summary>
+        /// tableSporen2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Table tableSporen2;
+        
+        /// <summary>
+        /// rowSporen2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableRow rowSporen2;
+        
+        /// <summary>
+        /// CellSpoor57 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell CellSpoor57;
+        
+        /// <summary>
+        /// TableSpoor38 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Table TableSpoor38;
+        
+        /// <summary>
+        /// CellSpoor56 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell CellSpoor56;
+        
+        /// <summary>
+        /// CellSpoor55 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell CellSpoor55;
+        
+        /// <summary>
+        /// CellSpoor54 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell CellSpoor54;
+        
+        /// <summary>
+        /// CellSpoor53 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell CellSpoor53;
+        
+        /// <summary>
+        /// CellSpoor52 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell CellSpoor52;
+        
+        /// <summary>
+        /// CellSpoor51 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell CellSpoor51;
+        
+        /// <summary>
+        /// CellSpoor64 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell CellSpoor64;
+        
+        /// <summary>
+        /// CellSpoor63 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell CellSpoor63;
+        
+        /// <summary>
+        /// CellSpoor62 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell CellSpoor62;
+        
+        /// <summary>
+        /// CellSpoor61 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell CellSpoor61;
+        
+        /// <summary>
+        /// CellSpoor74 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell CellSpoor74;
+        
+        /// <summary>
+        /// CellSpoor75 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell CellSpoor75;
+        
+        /// <summary>
+        /// CellSpoor76 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell CellSpoor76;
+        
+        /// <summary>
+        /// CellSpoor77 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell CellSpoor77;
+        
+        /// <summary>
+        /// CellExtraSporen1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell CellExtraSporen1;
+        
+        /// <summary>
+        /// CellExtraSporen2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell CellExtraSporen2;
+        
+        /// <summary>
+        /// Timer1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.Timer Timer1;
     }
 }
