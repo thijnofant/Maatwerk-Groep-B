996fc775d679bcb23898a960475dcb1a88492c14
i309262 <Sayalaugs@hotmail.com>
2015-01-07 10:33:33 +0100

    reserveren!

--- Software V1/RemiseSite Groep B/RemiseSite Groep B/Reserveren.aspx
+++ Software V1/RemiseSite Groep B/RemiseSite Groep B/Reserveren.aspx
@@ -1,3 +1,55 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Reserveren.aspx.cs" Inherits="RemiseSite_Groep_B.Reserveren" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <table class="nav-justified">
+        <tr>
+            <td style="width: 196px">
+    <asp:Label ID="Label1" runat="server" Text="Tramnummer"></asp:Label>
+            </td>
+            <td>
+    <asp:Label ID="Label2" runat="server" Text="Spoornummer"></asp:Label>
+            </td>
+        </tr>
+        <tr>
+            <td style="width: 196px">
+    <asp:TextBox ID="tbTramnr" runat="server" Height="30px"></asp:TextBox>
+            </td>
+            <td>
+    <asp:TextBox ID="tbSpoornr" runat="server" Height="30px"></asp:TextBox>
+            </td>
+        </tr>
+        <tr>
+            <td style="width: 196px">&nbsp;</td>
+            <td>
+    <asp:Button ID="btnBevestig" runat="server" OnClick="btnBevestig_Click" Text="Bevestig" />
+            </td>
+        </tr>
+    </table>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br />
+&nbsp;<asp:Panel ID="Panel1" runat="server" Width="799px">
+        <asp:CheckBox ID="cbReparatie" runat="server" OnCheckedChanged="cbReparatie_CheckedChanged" Text="Reparatie" />
+        <br />
+        <table class="nav-justified">
+            <tr>
+                <td style="width: 197px">
+                    <asp:Calendar ID="Calendar1" runat="server"></asp:Calendar>
+                </td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td style="width: 197px">Soort Reparatie</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td style="width: 197px">
+                    <asp:RadioButton ID="rbKlein" runat="server" Text="Klein" />
+                </td>
+                <td>
+                    <asp:RadioButton ID="rbGroot" runat="server" Text="Groot" />
+                </td>
+            </tr>
+        </table>
+        <br />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</asp:Panel>
+    <br />
+&nbsp;<asp:Label ID="lblMessage" runat="server" Text="..."></asp:Label>
 </asp:Content>
--- Software V1/RemiseSite Groep B/RemiseSite Groep B/Reserveren.aspx.cs
+++ Software V1/RemiseSite Groep B/RemiseSite Groep B/Reserveren.aspx.cs
@@ -9,9 +9,82 @@ namespace RemiseSite_Groep_B
 {
     public partial class Reserveren : System.Web.UI.Page
     {
+        DatabaseManager db = DatabaseManager.Instance;
+        
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            //Panel1.Visible = false;
+        }
 
+        protected void cbReparatie_CheckedChanged(object sender, EventArgs e)
+        {
+            if (Panel1.Visible == true)
+            {
+                Panel1.Visible = false;
+                this.Response.Redirect("~/Reserveren.aspx");
+            }
+            else
+            {
+                Panel1.Visible = true;
+                this.Response.Redirect("~/Reserveren.aspx");
+            }
+        }
+
+        protected void btnBevestig_Click(object sender, EventArgs e)
+        {
+            Classes.Tram tram = db.ZoekTram(Convert.ToInt32(tbTramnr.Text));//haalt tram op uit de database
+            int tesmp = db.GetSpoorIDByNr(Convert.ToInt32(tbSpoornr.Text));
+            if (db.TramReserveren(tram.Id, tesmp))//reserveerd de tram in de database
+            {
+                lblMessage.Text = "De reservering is aangemaakt.";
+            }
+            else
+            {
+                lblMessage.Text = "Er is iets misgegaan met het reserveren van de tram. Probeer het opnieuw.";
+            }
+            Classes.BeurtType type = Classes.BeurtType.Klein;
+            int aantalToegestaandeBeurten = 0;
+
+            //het onderstaande wordt enkel uitgevoerd wanneer er ook is gekozen reparatie
+            if (cbReparatie.Checked)
+            {
+                if (rbGroot.Checked)//voor grote reparatie
+                {
+                    type = Classes.BeurtType.Groot;
+                    aantalToegestaandeBeurten = 1;
+                }
+                if (rbKlein.Checked)//voor kleine reparatie
+                {
+                    type = Classes.BeurtType.Klein;
+                    aantalToegestaandeBeurten = 4;
+                }
+                
+                DateTime datum = Convert.ToDateTime(Calendar1.SelectedDate);//geseleceteerde datum
+                Classes.Onderhoud onderhoud = new Classes.Onderhoud(datum, db.GetInsertID("ID", "TRAM_BEURT") + 1, type, tram, DateTime.Now.AddDays(1.0));//nieuw onderhoud object.
+
+                if (tram != null)
+                {
+                    if (db.GetAantalBeurten(type.ToString(), "Onderhoud", datum, tram.Id) < aantalToegestaandeBeurten)// kijkt of aantal gebeurde beurten niet het aantaal toegestane beurten overschrijd
+                    {
+                        db.OnderhoudInvoeren(onderhoud);//maakt reparatie aan in de database
+                        lblMessage.Text = "De Tram is gereserveerd voor onderhoud.";
+                    }
+                    else
+                    {
+                        lblMessage.Text = "De geselecteerde tram kan niet meer worden ingepland op de geselecteerde dag";
+                    }
+                }
+                else
+                {
+                    lblMessage.Text = "De ingevoerde tram bestaat niet";
+                }
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Panel1.Visible = true;
         }
     }
 }
